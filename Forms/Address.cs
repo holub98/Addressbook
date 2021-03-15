9f650eb1537eb50fd79b459967b41d14cc178577
gholubowicz <holubowicz.g@gmail.com>
2021-03-15 18:21:19 +0100

    Dodaj pliki projektów.

---
+++ Forms/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace PS1.Forms
+{
+    public class Address
+    {   
+        [Display(Name="Twoja ulica")]
+        [StringLength(60, MinimumLength =5), Required(ErrorMessage ="Uzupełnij to pole")]
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using PS1.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        [BindProperty]
+        public Address Address { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+    }
+}
